8d0bbd18cba367d41dafe4b2e7925f19f9437c6a
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-07-14 01:14:18 -0400

    Fix YouTube sign-in bug

--- src/default-services.js
+++ src/default-services.js
@@ -13,6 +13,7 @@ module.exports = [
     url: 'https://youtube.com',
     color: '#ff0000',
     style: {},
+    userAgent: "Mozilla/5.0 (X11; Linux x86_64; rv:75.0) Gecko/20100101 Firefox/75.0",
     permissions: []
   },
   {
