4c3194484011df72807e0858dd3beb3cc54b8b51
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-07-14 03:51:39 -0400

    Manual restart required to enable services

--- src/menu.js
+++ src/menu.js
@@ -1,7 +1,7 @@
-const { Menu, shell } = require('electron');
-const prompt = require('electron-prompt');
-const path = require('path');
-const fs = require('fs');
+const { Menu, shell } = require("electron");
+const prompt = require("electron-prompt");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = (store, services, mainWindow, app, defaultUserAgent) => {
   var servicesMenuItems = [];
@@ -10,338 +10,337 @@ module.exports = (store, services, mainWindow, app, defaultUserAgent) => {
 
   if (services !== undefined) {
     // Menu with all services that can be clicked for easy switching
-    servicesMenuItems = services.map(service => ({
+    servicesMenuItems = services.map((service) => ({
       label: service.name,
       visible: !service.hidden,
       click() {
-        console.log('Changing URL To: ' + service.url);
+        console.log("Changing URL To: " + service.url);
         mainWindow.loadURL(service.url);
-        mainWindow.send('run-loader', service);
-      }
+        mainWindow.send("run-loader", service);
+      },
     }));
 
     // Menu for selecting default service (one which is opened on starting the app)
-    defaultServiceMenuItems = services.map(service => ({
+    defaultServiceMenuItems = services.map((service) => ({
       label: service.name,
-      type: 'checkbox',
-      checked: store.get('options.defaultService')
-          ? store.get('options.defaultService') == service.name
-          : false,
+      type: "checkbox",
+      checked: store.get("options.defaultService")
+        ? store.get("options.defaultService") == service.name
+        : false,
       click(e) {
-        e.menu.items.forEach(e => {
+        e.menu.items.forEach((e) => {
           if (!(e.label === service.name)) e.checked = false;
         });
-        store.set('options.defaultService', service.name);
-      }
+        store.set("options.defaultService", service.name);
+      },
     }));
 
     // Menu with all services that can be clicked for easy switching
-    enabledServicesMenuItems = services.map(service => ({
+    enabledServicesMenuItems = services.map((service) => ({
       label: service.name,
-      type: 'checkbox',
+      type: "checkbox",
       checked: !service.hidden,
       click() {
-        if(service._defaultService) {
-          let currServices = store.get('services');
+        if (service._defaultService) {
+          let currServices = store.get("services");
           currServices.push({
             name: service.name,
-            hidden: !service.hidden
+            hidden: !service.hidden,
           });
           services = currServices;
-          store.set('services', currServices);
+          store.set("services", currServices);
         } else {
-          let currServices = store.get('services');
-          let currService = currServices.find(s => service.name == s.name);
-          currService.hidden = service.hidden ? undefined : true
+          let currServices = store.get("services");
+          let currService = currServices.find((s) => service.name == s.name);
+          currService.hidden = service.hidden ? undefined : true;
           services = currServices;
-          store.set('services', currServices);
+          store.set("services", currServices);
         }
-      }
+      },
     }));
   }
 
   return Menu.buildFromTemplate([
     {
-      label: 'Conflux',
+      label: "Conflux",
       submenu: [
-        { label: 'Conflux (' + app.getVersion() + ')', enabled: false },
-        { label: 'Developed By OOF2510', enabled: false },
-	      { label: 'Forked from oscartbeaumont/ElectronPlayer', enabled: false },
+        { label: "Conflux (" + app.getVersion() + ")", enabled: false },
+        { label: "Developed By OOF2510", enabled: false },
+        { label: "Forked from oscartbeaumont/ElectronPlayer", enabled: false },
         {
-          label: 'Quit Conflux',
-          accelerator: 'Command+Q', // TODO: Non Mac Shortcut
+          label: "Quit Conflux",
+          accelerator: "Command+Q", // TODO: Non Mac Shortcut
           click() {
             app.quit();
-          }
-        }
-      ]
+          },
+        },
+      ],
     },
     {
-      label: 'Services',
+      label: "Services",
       submenu: [
         {
-          label: 'Menu',
-          accelerator: 'CmdOrCtrl+H',
+          label: "Menu",
+          accelerator: "CmdOrCtrl+H",
           click() {
-            console.log('Change To The Menu');
+            console.log("Change To The Menu");
             mainWindow.webContents.userAgent = defaultUserAgent;
-            mainWindow.loadFile('src/ui/index.html');
-          }
+            mainWindow.loadFile("src/ui/index.html");
+          },
         },
         {
-          label: 'Custom Url',
-          accelerator: 'CmdOrCtrl+O',
+          label: "Custom Url",
+          accelerator: "CmdOrCtrl+O",
           click() {
             prompt({
-              title: 'Open Custom URL',
-              label: 'URL:',
+              title: "Open Custom URL",
+              label: "URL:",
               inputAttrs: {
-                  type: 'url',
-                  placeholder: 'http://example.org'
+                type: "url",
+                placeholder: "http://example.org",
               },
-              alwaysOnTop: true
-          })
-          .then(inputtedURL => {
-            if (inputtedURL != null) {
-              if(inputtedURL == '') {
-                inputtedURL = 'http://example.org';
-              }
+              alwaysOnTop: true,
+            })
+              .then((inputtedURL) => {
+                if (inputtedURL != null) {
+                  if (inputtedURL == "") {
+                    inputtedURL = "http://example.org";
+                  }
 
-              console.log('Opening Custom URL: ' + inputtedURL);
-              mainWindow.loadURL(inputtedURL);
-            }
-          })
-          .catch(console.error);
-          }
-        }
-      ].concat(servicesMenuItems)
+                  console.log("Opening Custom URL: " + inputtedURL);
+                  mainWindow.loadURL(inputtedURL);
+                }
+              })
+              .catch(console.error);
+          },
+        },
+      ].concat(servicesMenuItems),
     },
     {
-      label: 'Settings',
+      label: "Settings",
       submenu: [
         {
-          label: 'Always On Top',
-          type: 'checkbox',
+          label: "Always On Top",
+          type: "checkbox",
           click(e) {
-            store.set('options.alwaysOnTop', e.checked);
+            store.set("options.alwaysOnTop", e.checked);
             mainWindow.setAlwaysOnTop(e.checked);
           },
-          checked: store.get('options.alwaysOnTop')
+          checked: store.get("options.alwaysOnTop"),
         },
         {
-          label: 'Frameless Window *',
-          type: 'checkbox',
+          label: "Frameless Window *",
+          type: "checkbox",
           click(e) {
-            store.set('options.hideWindowFrame', e.checked);
-            app.emit('relaunch');
+            store.set("options.hideWindowFrame", e.checked);
+            app.emit("relaunch");
           },
-          checked: store.get('options.hideWindowFrame')
-            ? store.get('options.hideWindowFrame')
-            : false
-        },
-        {
-          label: 'Remember Window Details',
-          type: 'checkbox',
-          click(e) {
-            if (store.get('options.windowDetails')) {
-              store.delete('options.windowDetails');
-            } else {
-              store.set('options.windowDetails', {});
-            }
-          },
-          checked: !!store.get('options.windowDetails')
-        },
-        {
-          label: 'Picture In Picture (Mac Only) *',
-          type: 'checkbox',
-          click(e) {
-            store.set('options.pictureInPicture', e.checked);
-            app.emit('relaunch');
-          },
-          checked: store.get('options.pictureInPicture')
-            ? store.get('options.pictureInPicture')
+          checked: store.get("options.hideWindowFrame")
+            ? store.get("options.hideWindowFrame")
             : false,
-          visible: process.platform === 'darwin'
         },
         {
-          label: 'Adblock *',
-          type: 'checkbox',
+          label: "Remember Window Details",
+          type: "checkbox",
           click(e) {
-            store.set('options.adblock', e.checked);
+            if (store.get("options.windowDetails")) {
+              store.delete("options.windowDetails");
+            } else {
+              store.set("options.windowDetails", {});
+            }
+          },
+          checked: !!store.get("options.windowDetails"),
+        },
+        {
+          label: "Picture In Picture (Mac Only) *",
+          type: "checkbox",
+          click(e) {
+            store.set("options.pictureInPicture", e.checked);
+            app.emit("relaunch");
+          },
+          checked: store.get("options.pictureInPicture")
+            ? store.get("options.pictureInPicture")
+            : false,
+          visible: process.platform === "darwin",
+        },
+        {
+          label: "Adblock *",
+          type: "checkbox",
+          click(e) {
+            store.set("options.adblock", e.checked);
 
             // Store details to remeber when relaunched
-            if (mainWindow.getURL() != '') {
-              store.set('relaunch.toPage', mainWindow.getURL());
+            if (mainWindow.getURL() != "") {
+              store.set("relaunch.toPage", mainWindow.getURL());
             }
-            store.set('relaunch.windowDetails', {
+            store.set("relaunch.windowDetails", {
               position: mainWindow.getPosition(),
-              size: mainWindow.getSize()
+              size: mainWindow.getSize(),
             });
 
             // Restart the app
             app.relaunch();
             app.quit();
           },
-          checked: store.get('options.adblock')
-            ? store.get('options.adblock')
-            : false
+          checked: store.get("options.adblock")
+            ? store.get("options.adblock")
+            : false,
         },
         {
-          label: 'BetterTTV *',
-          type: 'checkbox',
+          label: "BetterTTV *",
+          type: "checkbox",
           click(e) {
-            store.set('options.bttv', e.checked);
+            store.set("options.bttv", e.checked);
 
             // Store details to remeber when relaunched
-            if (mainWindow.getURL() != '') {
-              store.set('relaunch.toPage', mainWindow.getURL());
+            if (mainWindow.getURL() != "") {
+              store.set("relaunch.toPage", mainWindow.getURL());
             }
-            store.set('relaunch.windowDetails', {
+            store.set("relaunch.windowDetails", {
               position: mainWindow.getPosition(),
-              size: mainWindow.getSize()
+              size: mainWindow.getSize(),
             });
 
             // Restart the app
             app.relaunch();
             app.quit();
           },
-          checked: store.get('options.bttv')
-            ? store.get('options.bttv')
-            : false
+          checked: store.get("options.bttv")
+            ? store.get("options.bttv")
+            : false,
         },
         {
-          label: 'Start in Fullscreen',
-          type: 'checkbox',
+          label: "Start in Fullscreen",
+          type: "checkbox",
           click(e) {
-            store.set('options.launchFullscreen', e.checked);
+            store.set("options.launchFullscreen", e.checked);
           },
-          checked: store.get('options.launchFullscreen')
-            ? store.get('options.launchFullscreen')
-            : false
+          checked: store.get("options.launchFullscreen")
+            ? store.get("options.launchFullscreen")
+            : false,
         },
         {
-          label: 'Enabled Services',
-          submenu: enabledServicesMenuItems
+          label: "` Enabled Services",
+          submenu: enabledServicesMenuItems,
         },
         {
-          label: 'Default Service',
+          label: "Default Service",
           submenu: [
             {
-              label: 'Menu',
-              type: 'checkbox',
+              label: "Menu",
+              type: "checkbox",
               click(e) {
-                e.menu.items.forEach(e => {
-                  if (!(e.label === 'Menu')) e.checked = false;
+                e.menu.items.forEach((e) => {
+                  if (!(e.label === "Menu")) e.checked = false;
                 });
-                store.delete('options.defaultService');
+                store.delete("options.defaultService");
               },
-              checked: store.get('options.defaultService') === undefined
+              checked: store.get("options.defaultService") === undefined,
             },
             {
-              label: 'Last Opened Page',
-              type: 'checkbox',
+              label: "Last Opened Page",
+              type: "checkbox",
               click(e) {
-                e.menu.items.forEach(e => {
-                  if (!(e.label === 'Last Opened Page')) e.checked = false;
+                e.menu.items.forEach((e) => {
+                  if (!(e.label === "Last Opened Page")) e.checked = false;
                 });
-                store.set('options.defaultService', 'lastOpenedPage');
+                store.set("options.defaultService", "lastOpenedPage");
               },
-              checked: store.get('options.defaultService') === 'lastOpenedPage'
+              checked: store.get("options.defaultService") === "lastOpenedPage",
             },
-            { type: 'separator' }
-          ].concat(defaultServiceMenuItems)
+            { type: "separator" },
+          ].concat(defaultServiceMenuItems),
         },
         {
-          label: 'Edit Config',
+          label: "Edit Config",
           click() {
             store.openInEditor();
-          }
+          },
         },
         {
-          label: 'Reset all settings *',
+          label: "Reset all settings *",
           click() {
             // Reset Config
             store.clear();
 
             // Clear Engine Cache
             let engineCachePath = path.join(
-              app.getPath('userData'),
-              'adblock-engine-cache.txt'
+              app.getPath("userData"),
+              "adblock-engine-cache.txt"
             );
             fs.unlinkSync(engineCachePath);
 
             // Restart the app
             app.relaunch();
             app.quit();
-          }
+          },
         },
-        { label: '* Means App Will Restart', enabled: false }
-      ]
+        { label: "* Means App Will Restart", enabled: false },
+        { label: "` Means Manual Restart is Required to Take Effect", enabled: false },
+      ],
     },
     {
-      label: 'Edit',
+      label: "Edit",
       submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' },
-        { role: 'pasteandmatchstyle' },
-        { role: 'delete' },
-        { role: 'selectall' }
-      ]
+        { role: "undo" },
+        { role: "redo" },
+        { type: "separator" },
+        { role: "cut" },
+        { role: "copy" },
+        { role: "paste" },
+        { role: "pasteandmatchstyle" },
+        { role: "delete" },
+        { role: "selectall" },
+      ],
     },
     {
-      label: 'Developer',
+      label: "Developer",
       submenu: [
         {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
+          label: "Reload",
+          accelerator: "CmdOrCtrl+R",
           click(item, focusedWindow) {
             if (focusedWindow) focusedWindow.reload();
-          }
+          },
         },
         {
-          label: 'Toggle Developer Tools',
+          label: "Toggle Developer Tools",
           accelerator:
-            process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+            process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
           click(item, focusedWindow) {
             focusedWindow.webContents.toggleDevTools();
-          }
+          },
         },
         {
-          type: 'separator'
+          type: "separator",
         },
         {
-          role: 'resetzoom'
+          role: "resetzoom",
         },
         {
-          role: 'zoomin'
+          role: "zoomin",
         },
         {
-          role: 'zoomout'
+          role: "zoomout",
         },
         {
-          type: 'separator'
+          type: "separator",
         },
         {
-          role: 'togglefullscreen'
-        }
-      ]
+          role: "togglefullscreen",
+        },
+      ],
     },
     {
-      role: 'help',
+      role: "help",
       submenu: [
         {
-          label: 'More Information',
+          label: "More Information",
           click() {
-            shell.openExternal(
-              'http://github.com/oof2510/Conflux'
-            );
-          }
-        }
-      ]
-    }
+            shell.openExternal("http://github.com/oof2510/Conflux");
+          },
+        },
+      ],
+    },
   ]);
 };
